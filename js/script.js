a28fc57970ec8f7ddb606f8425444633bd42f5da
SANYA <sbeakjib@gmail.com>
2024-12-18 00:13:05 +0700

    home selection has done

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
